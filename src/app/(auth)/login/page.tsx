e0bf52e53ab9deefcc92dcf734411e1839a40423
David <dbarron410@vt.edu>
2025-01-21 10:22:44 -0500

    remove pageprops

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 import {
- Card,
- CardHeader,
- CardTitle,
- CardDescription,
- CardContent,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
@@ -12,74 +12,73 @@ import { login, signup } from "./actions";
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 
-type PageProps = {
- params: { slug: string };
- searchParams: { [key: string]: string | string[] | undefined };
-}
+export default async function Login({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>
+}) {
+  const supabase = await createClient();
 
-export default async function Login({ searchParams }: PageProps) {
- const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
- const {
-   data: { user },
- } = await supabase.auth.getUser();
+  if (user) {
+    return redirect("/");
+  }
 
- if (user) {
-   return redirect("/");
- }
+  const message = searchParams.message as string | undefined;
 
- const message = searchParams.message as string | undefined;
-
- return (
-   <section className="h-[calc(55vh-57px)] flex justify-center items-center">
-     <Card className="mx-auto max-w-sm">
-       <CardHeader>
-         <CardTitle className="text-2xl">Login</CardTitle>
-         <CardDescription>
-           Enter your email below to login to your account
-         </CardDescription>
-       </CardHeader>
-       <CardContent className="flex flex-col gap-4">
-         <form id="login-form" className="grid gap-4">
-           <div className="grid gap-2">
-             <Label htmlFor="email">Email</Label>
-             <Input
-               id="email"
-               name="email"
-               type="email"
-               placeholder="m@example.com"
-               required
-             />
-           </div>
-           <div className="grid gap-2">
-             <div className="flex items-center">
-               <Label htmlFor="password">Password</Label>
-             </div>
-             <Input
-               minLength={6}
-               name="password"
-               id="password"
-               type="password"
-               required
-             />
-           </div>
-           {message && (
-             <div className="text-sm font-medium text-destructive">
-               {message}
-             </div>
-           )}
-           <Button formAction={login} className="w-full">
-             Login
-           </Button>
-         </form>
-         <div className="text-center text-sm">
-           Don&apos;t have an account?{" "}
-           <button formAction={signup} form="login-form" className="underline">
-             Sign up
-           </button>
-         </div>
-       </CardContent>
-     </Card>
-   </section>
- );
+  return (
+    <section className="h-[calc(55vh-57px)] flex justify-center items-center">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-4">
+          <form id="login-form" className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input
+                minLength={6}
+                name="password"
+                id="password"
+                type="password"
+                required
+              />
+            </div>
+            {message && (
+              <div className="text-sm font-medium text-destructive">
+                {message}
+              </div>
+            )}
+            <Button formAction={login} className="w-full">
+              Login
+            </Button>
+          </form>
+          <div className="text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <button formAction={signup} form="login-form" className="underline">
+              Sign up
+            </button>
+          </div>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
